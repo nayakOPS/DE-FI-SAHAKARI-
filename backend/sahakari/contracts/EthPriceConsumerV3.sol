561ffa6d41ba49675efa8366af3ce116638ba6d2
Binayak <kunonner2002@gmail.com>
2024-06-25 21:57:14 +0545

    Changes made to ETH/USD pricefeeder contract

--- backend/sahakari/contracts/EthPriceConsumerV3.sol
+++ backend/sahakari/contracts/EthPriceConsumerV3.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.26;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+// Select Sepolia - Testnet for this to run
 contract EthPriceConsumerV3 {
     AggregatorV3Interface internal priceFeed;
 
@@ -23,6 +24,10 @@ contract EthPriceConsumerV3 {
      */
     function getLatestPrice() public view returns (int) {
         (, int price, , , ) = priceFeed.latestRoundData();
+
+        // Debugging: Check if the data is being fetched correctly
+        require(price > 0, "Invalid price returned from Chainlink");
+        
         return price;
     }
 }
